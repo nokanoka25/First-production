a25f13af2a2ef88967f61b318e78f795e41f84d4
加野聖也 <seiyakano@kanokiyoshinarinoMacBook-Pro.local>
2025-01-22 15:43:43 +0900

    fix/マイページキャンプギア削除を正常化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
   # private
   # def not_authenticated
   # redirect_to login_path, alert: "Please login first"
