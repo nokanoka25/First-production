66d17abb9704fd95c4ca8565bd5756ae3db6e752
加野聖也 <seiyakano@kanokiyoshinarinoMacBook-Pro.local>
2025-05-22 01:29:09 +0900

    fix/DBとコンテナの整理

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module Myapp
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-    config.active_job.queue_adapter = :sidekiq
   
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
